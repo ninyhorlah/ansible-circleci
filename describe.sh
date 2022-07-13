7d17ac1b83ca6cf4622fc027a784b430571d4d48
esther ninyo <ninyhorlah6@gmail.com>
2022-07-13 19:23:47 +0100

    add new ip

--- describe.sh
+++ describe.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-echo [all] > inventory 
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text >> inventory
+echo [all] > inventory.txt 
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text >> inventory.txt
